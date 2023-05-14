e761bdd992e87f6180ac5ac72b0cf29edaf03368
Marios Kanellopoulos <mariosknl@outlook.com>
2023-05-14 17:54:34 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// Routes
+import Dashboard, { dashboardLoader } from "./pages/Dashboard";
+import Error from "./pages/Error";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Dashboard />,
+		loader: dashboardLoader,
+		errorElement: <Error />,
+	},
+]);
+
+function App() {
+	return (
+		<div className="App">
+			<RouterProvider router={router} />
+		</div>
+	);
+}
+
+export default App;
